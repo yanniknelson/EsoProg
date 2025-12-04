c67c5258c9ed6b4f18e26a8437a121ac2bec617e
yanniknelson <yannikdanielnelson@pm.me>
2025-12-04 23:44:55 +0000

    Created project for abstract syntax tree framework #5

---
+++ Core/IAbstractSyntaxTree/Build-IAbstractSyntaxTree.lua
@@ -0,0 +1,45 @@
+project "IAbstractSyntaxTree"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "Src/**.h", "Src/**.cpp" }
+
+   includedirs
+   {
+      "Src",
+
+	  -- Include Core
+      "%{wks.location}/Core/SmartEnums/Src",
+	  "%{wks.location}/Core/ELanguages/Src",
+	  "%{wks.location}/Core/ITokeniser/Src",
+	  "%{wks.location}/Core/ImGuiHelpers/ImGuiValueChangeCallbacks",
+
+      -- Include externals
+      "%{wks.location}/%{externals.spdlog}/include"
+   }
+
+   targetdir ("%{wks.location}/bin/%{prj.name}/" .. OutputDir)
+   objdir ("%{wks.location}/bin/%{prj.name}/" .. OutputDir .. "/Intermediates")
+
+   filter "system:windows"
+       systemversion "latest"
+       defines { }
+
+   filter "configurations:Debug"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
+
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+   filter "configurations:Dist"
+       defines { "DIST" }
+       runtime "Release"
+       optimize "On"
+       symbols "Off"
---
+++ Core/IAbstractSyntaxTree/Src/IAbstractSyntaxTree.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <ITokeniser.h>
+
+template<typename TokenClass>
+class IAST
+{
+public:
+
+	void Parse();
+
+protected:
+	ITokeniser<TokenClass> m_tokeniser;
+};
