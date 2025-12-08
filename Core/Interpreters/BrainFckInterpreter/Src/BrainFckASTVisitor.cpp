847d5e3641ca06d0037d36bf904e7de8a7e24a1e
yanniknelson <yannikdanielnelson@pm.me>
2025-12-08 15:49:20 +0000

    Split Printing Visitor into cpp

--- Core/Interpreters/BrainFckInterpreter/Src/BrainFckASTVisitor.cpp
+++ Core/Interpreters/BrainFckInterpreter/Src/BrainFckASTVisitor.cpp
@@ -1,1 +1,65 @@
 #include "BrainFckASTVisitor.h"
+
+void BrainFckPrintingVisitor::Traverse(TProgramPtr pProgram)
+{
+	Print("Program");
+	m_depth = 1;
+	for (BrainFckOperationPtr pOperation : pProgram->m_region)
+	{
+		Traverse(pOperation);
+	}
+	m_depth = 0;
+	Print("Program End");
+};
+
+void BrainFckPrintingVisitor::Traverse(BrainFckOperationPtr pOperation)
+{
+	switch (pOperation->GetType())
+	{
+	case(BrainFckOperationTypes::Loop):
+	{
+		Traverse(std::dynamic_pointer_cast<Loop>(pOperation));
+		break;
+	}
+	case(BrainFckOperationTypes::IncOp): [[fallthrough]];
+	case(BrainFckOperationTypes::DecOp): [[fallthrough]];
+	case(BrainFckOperationTypes::LeftOp): [[fallthrough]];
+	case(BrainFckOperationTypes::RightOp): [[fallthrough]];
+	case(BrainFckOperationTypes::InOp): [[fallthrough]];
+	case(BrainFckOperationTypes::OutOp): [[fallthrough]];
+	{
+		Visit(pOperation);
+		break;
+	}
+	default:
+	{
+		std::cout << "Error" << std::endl;
+	}
+	}
+}
+
+void BrainFckPrintingVisitor::Visit(BrainFckOperationPtr pOperation)
+{
+	Print(BrainFckOperationTypes::ToString(pOperation->GetType()));
+};
+
+void BrainFckPrintingVisitor::Traverse(std::shared_ptr<Loop> pLoop)
+{
+	Print("Loop Start");
+	m_depth += 1;
+	for (BrainFckOperationPtr pOperation : pLoop->m_region)
+	{
+		Traverse(pOperation);
+	}
+	m_depth -= 1;
+	Print("Loop End");
+}
+
+void BrainFckPrintingVisitor::Print(const char* str)
+{
+	for (size_t i = 0; i < m_depth; i++)
+	{
+		std::cout << "\t";
+	}
+	std::cout << str << "\n";
+}
--- Core/Interpreters/BrainFckInterpreter/Src/BrainFckASTVisitor.h
+++ Core/Interpreters/BrainFckInterpreter/Src/BrainFckASTVisitor.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <IASTVisitor.h>
+#pragma once
+
 #include<BrainFckAST.h>
 #include<memory>
 #include<iostream>
@@ -9,68 +11,14 @@ class BrainFckPrintingVisitor : public IASTVisitor<BrainFckProgram>
 {
 	using TProgramPtr = std::shared_ptr<BrainFckProgram>;
 public:
-	virtual void Traverse(TProgramPtr pProgram) override
-	{
-		m_depth = 0;
-		print("Program");
-		for (BrainFckOperationPtr pOperation : pProgram->m_region)
-		{
-			Traverse(pOperation);
-		}
-		print("Program End");
-	};
+	virtual void Traverse(TProgramPtr pProgram) override;
 
-	void Traverse(BrainFckOperationPtr pOperation)
-	{
-		switch (pOperation->GetType())
-		{
-		case(BrainFckOperationTypes::Loop):
-		{
-			Traverse(std::dynamic_pointer_cast<Loop>(pOperation));
-			break;
-		}
-		case(BrainFckOperationTypes::IncOp): [[fallthrough]];
-		case(BrainFckOperationTypes::DecOp): [[fallthrough]];
-		case(BrainFckOperationTypes::LeftOp): [[fallthrough]];
-		case(BrainFckOperationTypes::RightOp): [[fallthrough]];
-		case(BrainFckOperationTypes::InOp): [[fallthrough]];
-		case(BrainFckOperationTypes::OutOp): [[fallthrough]];
-		{
-			Visit(pOperation);
-			break;
-		}
-		default:
-		{
-			std::cout << "Error" << std::endl;
-		}
-		}
-	}
+	void Traverse(BrainFckOperationPtr pOperation);
+	void Visit(BrainFckOperationPtr pOperation);
+	void Traverse(std::shared_ptr<Loop> pLoop);
 
-	void Visit(BrainFckOperationPtr pOperation)
-	{
-		print(BrainFckOperationTypes::ToString(pOperation->GetType()));
-	};
-
-	void Traverse(std::shared_ptr<Loop> pLoop)
-	{
-		print("Loop Start");
-		m_depth += 1;
-		for (BrainFckOperationPtr pOperation : pLoop->m_region)
-		{
-			Traverse(pOperation);
-		}
-		m_depth -= 1;
-		print("Loop End");
-	}
 private:
 	size_t m_depth = 0;
 
-	void print(const char* str)
-	{
-		for (size_t i = 0; i < m_depth; i++)
-		{
-			std::cout << "\t";
-		}
-		std::cout << str << "\n";
-	}
+	void Print(const char* str);
 };
