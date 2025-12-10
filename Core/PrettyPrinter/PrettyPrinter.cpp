02ffcd49d458cc3beb846dd25b8f70946003b044
guneshsn_ <hasanxgg1@gmail.com>
2025-12-10 14:45:57 +0300

    added PrettyPrinter.cpp

---
+++ Core/PrettyPrinter/PrettyPrinter.cpp
@@ -0,0 +1,96 @@
+#include "PrettyPrinter.h"
+#include <sstream>
+
+namespace
+{
+    const std::string INDENT = "\t";
+}
+
+bool CPrettyPrinter::IsSameGroup(char a, char b)
+{
+    return (a == b);
+}
+
+bool CPrettyPrinter::IsNewlineNeeded(char current, char next)
+{
+    if ((current == '.' && next != '.') || (current == ',' && next != ','))
+    {
+        return true;
+    }
+    
+    if ((current == '+' || current == '-') && 
+        next != '.' && next != ',' && next != current)
+    {
+        return true;
+    }
+    
+    if ((current == '>' && next == '<') || 
+        (current == '<' && next == '>'))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+bool CPrettyPrinter::IsIndentChange(char c)
+{
+    return (c == '[' || c == ']');
+}
+
+std::string CPrettyPrinter::FormatBrainFck(const std::string& code)
+{
+    if (code.empty())
+    {
+        return "";
+    }
+    
+    std::stringstream output;
+    int indentLevel = 0;
+    bool newLinePending = false;
+    
+    for (size_t i = 0; i < code.size(); ++i)
+    {
+        char current = code[i];
+        
+        if (current == ']' && indentLevel > 0)
+        {
+            --indentLevel;
+        }
+        
+        if (newLinePending)
+        {
+            output << '\n';
+            if (current != ']')
+            {
+                for (int j = 0; j < indentLevel; ++j)
+                {
+                    output << INDENT;
+                }
+            }
+            newLinePending = false;
+        }
+        
+        output << current;
+        
+        if (current == '[')
+        {
+            ++indentLevel;
+            newLinePending = true;
+        }
+        else if (current == ']')
+        {
+            newLinePending = true;
+        }
+        else if (i < code.size() - 1)
+        {
+            char next = code[i + 1];
+            if (IsNewlineNeeded(current, next))
+            {
+                newLinePending = true;
+            }
+        }
+    }
+    
+    return output.str();
+}
