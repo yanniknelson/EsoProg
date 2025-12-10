58251a5ec29d536d8173c52d3cb348615b71b82e
guneshsn_ <hasanxgg1@gmail.com>
2025-12-10 15:16:55 +0300

    new comments added for readability

--- Core/PrettyPrinter/PrettyPrinter.h
+++ Core/PrettyPrinter/PrettyPrinter.h
@@ -8,7 +8,10 @@ public:
     static std::string FormatBrainFck(const std::string& code);
     
 private:
+    //Checks if two characters belong to the same group
     static bool IsSameGroup(char a, char b);
+    //Determines if a newline is needed between two characters
     static bool IsNewlineNeeded(char current, char next);
+    //Checks if a character affects indentation
     static bool IsIndentChange(char c);
 };
